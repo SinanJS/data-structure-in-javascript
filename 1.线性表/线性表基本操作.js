913c4d3f9cb2803f4ebdba772e762fb35dd86f80
SinanJS <titan_js@163.com>
2016-06-19 10:15:33 +0800

    修改错误

--- 1.线性表/线性表基本操作.js
+++ 1.线性表/线性表基本操作.js
@@ -6,31 +6,31 @@ var List = function () {
 
 };
 List.prototype = {
-    //初始化操作，建立一个空的线性表L
-    initList: function () {
-        var list = {};
-        return list;
-    },
-    //若线性表为空的，返回false,否则返回true
-    isListEmpty: function (list) {
-        var hasProp = false;
-        for (var item in list) {
-            hasProp = true;
-            break;
-        }
-        if (hasProp) {
-            return true; //不空
-        } else {
-            return false; //空的   
-        }
-    },
-    //清空线性表
-    clearList: function (list) {
-        list = {};
-    },
-    //将线性表L中的第i个位置元素值返回给e
-    getElem: function () {
+	//初始化操作，建立一个空的线性表L
+	initList: function () {
+		var list = {};
+		return list;
+	},
+	//若线性表为空的，返回false,否则返回true
+	isListEmpty: function (list) {
+		var hasProp = false;
+		for (var item in list) {
+			hasProp = true;
+			break;
+		}
+		if (hasProp) {
+			return true; //不空
+		} else {
+			return false; //空的   
+		}
+	},
+	//清空线性表
+	clearList: function (list) {
+		list = {};
+	},
+	//将线性表L中的第i个位置元素值返回给e
+	getElem: function () {
 
-    }
+	}
 
 }
